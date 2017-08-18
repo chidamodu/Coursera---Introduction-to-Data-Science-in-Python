10b79bbe9c866874f397c8d8ef6c66fb5ef79ceb
chidamodu <chidambaram.spv@gmail.com>
2017-08-18 00:56:50 -0700

    Update Assignment 2-Pandas.py

--- Assignment 2-Pandas.py
+++ Assignment 2-Pandas.py
@@ -2,20 +2,20 @@ import pandas as pd
 df = pd.read_csv('olympics.csv', index_col=0, skiprows=1) #reading the file 'olympics.csv' by setting the column at 0th position as index column and skipping the first row
 for col in df.columns: #looping through columns to replace column names
     if col[:2]=='01': #checking if first two characters are equal to '01'. If yes then go to next step of renaming the column name
-        df.rename(columns={col:'Gold'+col[4:]}, inplace=True) #the first four characters, starting from 0th position to ending at 3rd position, are replaced with 'Gold' and the remaining characters are inplace means:reciprocate the change in the dataset rather than returning a copy
-    if col[:2]=='02':
-        df.rename(columns={col:'Silver'+col[4:]}, inplace=True)
-    if col[:2]=='03':
-        df.rename(columns={col:'Bronze'+col[4:]}, inplace=True)
-    if col[:1]=='№':
-        df.rename(columns={col:'#'+col[1:]}, inplace=True)
+        df.rename(columns={col:'Gold'+col[4:]}, inplace=True) #the first four characters, starting from 0th position to ending at 3rd position, are replaced with 'Gold' and the remaining characters are added to the new first four characters. inplace means:reciprocate the change in the dataset rather than returning a copy
+    if col[:2]=='02':#checking if first two characters are equal to '02'. If yes then go to next step
+        df.rename(columns={col:'Silver'+col[4:]}, inplace=True) #as explained above the first four characters of the corresponding column name get substituted
+    if col[:2]=='03':#checking if first two characters are equal to '01'. If yes then go to next step
+        df.rename(columns={col:'Bronze'+col[4:]}, inplace=True) #similar procedure as explained above
+    if col[:1]=='№':#similar procedure as explained above but only one character is verified equal or not to a specified value
+        df.rename(columns={col:'#'+col[1:]}, inplace=True) #only the first character is replaced with # and the remaining characters are added to the new, chnaged, character
 
 names_ids = df.index.str.split('\s\(') # split the index by '('
 
 df.index = names_ids.str[0] # the [0] element is the country name (new index) 
 df['ID'] = names_ids.str[1].str[:3] # the [1] element is the abbreviation or ID (take first 3 characters from that)
-df = df.drop('Totals')
+df = df.drop('Totals') #column 'Totals' is dropped off the dataset not deleted though
 df['Country']=df.index
-df=df.set_index('Country')
-df=df.reset_index()
-df.head(5)
+df=df.set_index('Country') #here 'Country' is made as the index column
+df=df.reset_index() #by resetting index, column 'Country' is made as the first column starting at 0th position 
+df.head(5) #the first 5 records are displayed
